bc98ad1678a8db80275523e7681b85dc34c5f304
RotemEZ <111863561+RotemEZ@users.noreply.github.com>
2024-07-12 16:31:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ import scipy.cluster.hierarchy as sch
 spotify = pd.read_csv("spotify-2023.csv", encoding='ISO-8859-1')
 
 # Remove strange row
-spotify.drop(574,inplace=True)
-
 # Convert streams, in_shazam_charts and in_deezer_playlists columns to numeric type
 spotify["streams"] = pd.to_numeric(spotify['streams'], errors='coerce')
 spotify["in_deezer_playlists"] = pd.to_numeric(spotify['in_deezer_playlists'].str.replace(",", ""), errors='coerce')
@@ -17,12 +15,25 @@ spotify["in_shazam_charts"] = pd.to_numeric(spotify['in_shazam_charts'], errors=
 
 
 # Remove released_day column(not relevant) and key column(too many missing values)
-spotify.drop(columns=['released_day', "key"], inplace=True)
+# Remove instrumentalness_%, liveness_%, speechiness_% columns(not relevant a significant amount of low values)
+spotify.drop(columns=['released_day', "key", 'instrumentalness_%', 'liveness_%', 'speechiness_%'], inplace=True)
 
 # Convert each percentage column to a range between 0 and 1
-percentage_columns = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%']
+percentage_columns = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%']
 for column in percentage_columns:
     spotify[column] = spotify[column] / 100
+
+
+energy_bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+energy_labels = ['Very Low (0-0.2)', 'Low (0.2-0.4)', 'Medium (0.4-0.6)', 'High (0.6-0.8)', 'Very High (0.8-1.0)']
+valence_bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+valence_labels = ['Very Low (0-0.2)', 'Low (0.2-0.4)', 'Medium (0.4-0.6)', 'High (0.6-0.8)', 'Very High (0.8-1.0)']
+
+# Bin the energy and valence columns
+spotify['energy level'] = pd.cut(spotify['energy_%'], bins=energy_bins, labels=energy_labels)
+spotify['valence level'] = pd.cut(spotify['valence_%'], bins=valence_bins, labels=valence_labels)
+
+spotify.drop(columns=['energy_%', 'valence_%' ], inplace=True)
 
 # Fill all null values in the 'in_shazam_charts' column with 0
 spotify['in_shazam_charts'].fillna(0, inplace=True)
@@ -61,7 +72,7 @@ with col1:
     )
     bar_fig.update_layout(
         yaxis={'categoryorder': 'total ascending'},
-        height=400  # Reduce height
+        height=400  
     )
     bar_fig.update_traces(marker=dict(line=dict(color='#000000', width=1)))
     st.plotly_chart(bar_fig, use_container_width=True)
@@ -95,8 +106,8 @@ with col1:
         yaxis_categoryorder='array',
         yaxis_categoryarray=month_names[::-1],  # Reverse the order of the months
         yaxis_title=None,
-        height=700,
-        width=700  # Reduce height
+        height=900,
+        width=700  
     )
 
     st.plotly_chart(box_fig_month, use_container_width=True)
@@ -104,34 +115,24 @@ with col1:
 with col2:
     st.markdown('### Sum of Spotify Charts by Energy and Valence Combinations')
 
-    energy_bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    energy_labels = ['Very Low (0-0.2)', 'Low (0.2-0.4)', 'Medium (0.4-0.6)', 'High (0.6-0.8)', 'Very High (0.8-1.0)']
-    valence_bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
-    valence_labels = ['Very Low (0-0.2)', 'Low (0.2-0.4)', 'Medium (0.4-0.6)', 'High (0.6-0.8)', 'Very High (0.8-1.0)']
-
-    # Bin the energy and valence columns
-    spotify['energy_bins'] = pd.cut(spotify['energy_%'], bins=energy_bins, labels=energy_labels)
-    spotify['valence_bins'] = pd.cut(spotify['valence_%'], bins=valence_bins, labels=valence_labels)
-
     # Group by combinations of energy and valence bins and calculate the sum of charts
-    combination_charts_sum = spotify.groupby(['energy_bins', 'valence_bins'])['in_spotify_charts'].sum().reset_index()
+    combination_charts_sum = spotify.groupby(['energy level', 'valence level'])['in_spotify_charts'].sum().reset_index()
 
     # Create the line plot with sum of charts
     line_combination_fig_sum = px.line(
         combination_charts_sum,
-        x='energy_bins',
+        x='energy level',
         y='in_spotify_charts',
-        color='valence_bins',
-        labels={'energy_bins': 'Energy Levels', 'in_spotify_charts': 'Sum of Spotify Charts',
-                'valence_bins': 'Valence Levels'},
+        color='valence level',
+        labels={'energy level': 'Energy Levels', 'in_spotify_charts': 'Sum of Spotify Charts',
+                'valence level': 'Valence Levels'},
         markers=True,
         line_shape='linear'
     )
 
-    # Update the layout to expand the figure width and add horizontal scrollbar
+    # Update the layout to expand the figure width
     line_combination_fig_sum.update_layout(
-        width=600,  # Reduce width
-        height=550,  # Reduce height
+        width=1000, height=600,  
         xaxis=dict(
             title='Energy Levels',
             tickmode='array',
@@ -144,6 +145,7 @@ with col2:
             )
         )
     )
+
     st.plotly_chart(line_combination_fig_sum, use_container_width=True)
 
     st.markdown('### Song Attribute vs. Number of Spotify Playlists')
@@ -175,8 +177,8 @@ with col2:
         facet_fig.layout[f'xaxis{i + 1}']['title']['text'] = axis_title
 
     facet_fig.update_layout(
-        width=600,  # Reduce width
-        height=800  # Increase height to fit the three plots vertically
+        width=600,  
+        height=800  
     )
     st.plotly_chart(facet_fig)
 
@@ -219,7 +221,7 @@ heatmap_fig.update_layout(
         yaxis_title='Features',
         xaxis=dict(side='bottom', tickangle=45, tickfont=dict(size=10)),
         yaxis=dict(tickfont=dict(size=10)),
-        margin=dict(l=100, r=20, t=50, b=150)  # Increase the bottom margin
+        margin=dict(l=100, r=20, t=50, b=150)  
     )
 
 st.plotly_chart(heatmap_fig, use_container_width=True)
