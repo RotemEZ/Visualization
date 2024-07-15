27128b716a238e9f563b5ef3ee6624b60a980010
RotemEZ <111863561+RotemEZ@users.noreply.github.com>
2024-07-15 18:31:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ spotify["in_shazam_charts"] = pd.to_numeric(spotify['in_shazam_charts'], errors=
 
 # Remove released_day, artist_count, mode columns (not relevant), key column(too many missing values)
 # Remove instrumentalness_%, liveness_%, speechiness_% columns(not relevant a significant amount of low values)
-spotify.drop(columns=['released_day', "key", 'instrumentalness_%', 'liveness_%', 'speechiness_%', 'artist_count', 'mode'], inplace=True)
+spotify.drop(columns=['released_day', "key", 'instrumentalness_%', 'liveness_%', 'speechiness_%', 'artist_count',
+                      'mode'], inplace=True)
 
 # Convert each percentage column to a range between 0 and 1
 percentage_columns = ['danceability_%', 'valence_%', 'energy_%', 'acousticness_%']
@@ -33,7 +34,7 @@ valence_labels = ['Very Low (0-0.2)', 'Low (0.2-0.4)', 'Medium (0.4-0.6)', 'High
 spotify['energy level'] = pd.cut(spotify['energy_%'], bins=energy_bins, labels=energy_labels)
 spotify['valence level'] = pd.cut(spotify['valence_%'], bins=valence_bins, labels=valence_labels)
 
-spotify.drop(columns=['energy_%', 'valence_%' ], inplace=True)
+spotify.drop(columns=['energy_%', 'valence_%'], inplace=True)
 
 # Fill all null values in the 'in_shazam_charts' column with 0
 spotify['in_shazam_charts'].fillna(0, inplace=True)
@@ -44,14 +45,17 @@ spotify['released_month'] = pd.to_datetime(spotify['released_month'], format='%m
 
 # Set up the Streamlit app
 st.set_page_config(layout="wide")  # Set the layout to wide mode
-st.title('Spotify User Preferences Dashboard')
 
+# Title and description
+st.markdown("<h1 style='text-align: left;'>Spotify User Preferences Dashboard</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: left;'>This visualization explores the most streamed songs on Spotify. Spotify is a popular music streaming app with over 615 million monthly users, including 239 million paid subscribers. Understanding user preferences can provide insights into global entertainment culture and current musical trends, aiding business decisions for app managers, record companies, and musicians.</p>", unsafe_allow_html=True)
 
 # Define columns layout
-col1, empty, col2= st.columns([2, 0.1, 2])
+col1, empty, col2 = st.columns([2, 0.1, 2])
+
 with col1:
     st.markdown('### Top 10 Most Popular Artists by Streams')
-    # Most Popular Artists
+
     min_year, max_year = st.slider('Select Year Range for Bar Chart', min_value=int(spotify['released_year'].min()),
                                    max_value=int(spotify['released_year'].max()),
                                    value=(int(spotify['released_year'].min()), int(spotify['released_year'].max())))
@@ -61,6 +65,7 @@ with col1:
     artist_popularity = filtered_data_bar.groupby('artist(s)_name')['streams'].sum().reset_index()
     artist_popularity = artist_popularity.sort_values(by='streams', ascending=False).head(10)
     artist_popularity['streams_billions'] = artist_popularity['streams'] / 1e9
+
     bar_fig = px.bar(
         artist_popularity,
         x='streams_billions',
@@ -70,9 +75,18 @@ with col1:
         color='streams_billions',
         color_continuous_scale='Blues'  # Using color saturation with 'Blues' color scale
     )
+
     bar_fig.update_layout(
         yaxis={'categoryorder': 'total ascending'},
-        height=400
+        height=400,
+        xaxis_title=dict(
+            text='Total Streams (in billions)',
+            font=dict(size=17)
+        ),
+        yaxis_title=dict(
+            text='Artist',
+            font=dict(size=17)
+        )
     )
     bar_fig.update_traces(marker=dict(line=dict(color='#000000', width=1)))
     st.plotly_chart(bar_fig, use_container_width=True)
@@ -94,22 +108,29 @@ with col1:
         y='released_month',
         x='streams',
         category_orders={'released_month': selected_months},
-        color_discrete_sequence=['purple']
+        color_discrete_sequence=['purple'],
+        points=False
     )
 
     box_fig_month.update_layout(
         yaxis_tickangle=0,
         yaxis_categoryorder='array',
         yaxis_title=None,
+        xaxis_title=dict(
+            text='Streams',
+            font=dict(size=17)
+        ),
         height=850,
         width=700
     )
+    box_fig_month.update_xaxes(tickfont=dict(size=14))
+    box_fig_month.update_yaxes(tickfont=dict(size=14))
 
     st.plotly_chart(box_fig_month, use_container_width=True)
 
 with col2:
     st.markdown('### Average of Spotify Charts by Energy and Valence Combinations')
-    
+
     # Group by combinations of energy and valence bins and calculate the average of charts
     combination_charts_avg = spotify.groupby(['energy level', 'valence level'])[
         'in_spotify_charts'].mean().reset_index()
@@ -126,9 +147,8 @@ with col2:
         line_shape='linear'
     )
 
-    # Update the layout to expand the figure width
     line_combination_fig_avg.update_layout(
-        width=1000, height=700,
+        width=1000, height=800,
         xaxis=dict(
             title='Energy Levels',
             tickmode='array',
@@ -136,11 +156,27 @@ with col2:
             ticktext=['Very Low\n(0-0.2)', 'Low\n(0.2-0.4)', 'Medium\n(0.4-0.6)', 'High\n(0.6-0.8)',
                       'Very High\n(0.8-1.0)'],
             dtick=1,
+            tickangle=50,
             rangeslider=dict(
                 visible=True
-            )
+            ),
+            titlefont=dict(size=17)
+        ),
+        yaxis=dict(
+            title='Average of Spotify Charts',
+            titlefont=dict(size=17),
+            tickfont=dict(size=14)
+        ),
+        legend=dict(
+            title=dict(
+                text='Valence Levels',
+                font=dict(size=17)
+            ),
+            font=dict(size=14)
         )
     )
+    line_combination_fig_avg.update_xaxes(tickfont=dict(size=14))
+    line_combination_fig_avg.update_yaxes(tickfont=dict(size=14))
 
     st.plotly_chart(line_combination_fig_avg, use_container_width=True)
 
@@ -169,7 +205,6 @@ with col2:
     facet_fig.update_traces(marker=dict(size=5, color='black'))
     facet_fig.update_xaxes(matches=None, showticklabels=True)
 
-
     # Update x-axis titles to show the attribute name
     for i, axis in enumerate(facet_fig.layout.annotations):
         axis_title = facet_fig.layout.annotations[i]['text'].split('=')[1].capitalize().replace('_%', '')
@@ -179,7 +214,7 @@ with col2:
 
     facet_fig.update_layout(
         width=600,
-        height=800
+        height=700
     )
 
     st.plotly_chart(facet_fig, use_container_width=True)
@@ -195,19 +230,19 @@ columns = [
 
 correlation_matrix = spotify[columns].corr().round(2)
 
-    # Generate the linkage matrix for clustering
+
 linkage = sch.linkage(correlation_matrix, method='ward')
 
-    # Create a dendrogram to get the order of the features
+
 dendrogram = sch.dendrogram(linkage, labels=correlation_matrix.columns, no_plot=True)
 clustered_order = dendrogram['ivl']
 
-    # Reorder the correlation matrix
+
 correlation_matrix = correlation_matrix.loc[clustered_order, clustered_order]
 
 correlation_matrix = correlation_matrix.iloc[:, ::-1]
 
-    # Heatmap using Plotly Express with PuOr colorscale
+
 heatmap_fig = ff.create_annotated_heatmap(
         z=correlation_matrix.values,
         x=list(correlation_matrix.columns),
